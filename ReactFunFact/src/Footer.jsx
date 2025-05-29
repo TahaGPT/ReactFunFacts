f632961e2db60ce214e2f14f4b604157ce424be7
TahaGPT <tahaaahmed12115@gmail.com>
2025-05-29 13:53:55 +0500

    Web Components

---
+++ ReactFunFact/src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2023 ReactFunFact. All rights reserved.</small>
+        </footer>
+    )
+}
---
+++ ReactFunFact/src/Header.jsx
@@ -0,0 +1,8 @@
+import reactLogo from './assets/react.svg';
+export default function Header() {
+    return (
+        <header>
+            <img src= {reactLogo} alt="React Logo" width="40px" />
+        </header>
+    )
+}
